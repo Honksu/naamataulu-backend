c60f66c4a680b83ee3016f11cb6393285a79e141
Antti Kallonen <kallonen.antti@gmail.com>
2018-10-16 22:44:58 +0300

    Django postgres

--- src/naamataulu/naamataulu/settings.py
+++ src/naamataulu/naamataulu/settings.py
@@ -75,8 +75,9 @@ WSGI_APPLICATION = 'naamataulu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ['DATABASE_URL'],
+        'PORT': 5432,
     }
 }
 
