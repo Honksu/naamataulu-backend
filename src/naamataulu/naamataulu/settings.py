e6ac3dae61b3d0b5d0769ddf36dc81d339082fa2
Antti Kallonen <kallonen.antti@gmail.com>
2018-10-16 22:58:58 +0300

    DATABASES fix

--- src/naamataulu/naamataulu/settings.py
+++ src/naamataulu/naamataulu/settings.py
@@ -74,6 +74,8 @@ WSGI_APPLICATION = 'naamataulu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+DATABASES = {}
+
 DATABASES['default'] = dj_database_url.parse(os.environ['DATABASE_URL'])
 
 # Password validation
