e7c9c62eae37d8e379add4fc6ec9c270a2c666c3
Antti Kallonen <kallonen.antti@gmail.com>
2018-10-16 22:57:07 +0300

    requirements.txt dj_database_url

--- src/naamataulu/naamataulu/settings.py
+++ src/naamataulu/naamataulu/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,17 +74,7 @@ WSGI_APPLICATION = 'naamataulu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ['DATABASE_URL'],
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'PORT': 5432,
-    }
-}
-
+DATABASES['default'] = dj_database_url.parse(os.environ['DATABASE_URL'])
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
