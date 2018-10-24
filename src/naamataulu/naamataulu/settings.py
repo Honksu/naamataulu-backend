4d147ecb9c66c3239e3bded02f46185def02891e
Antti Kallonen <kallonen.antti@gmail.com>
2018-10-24 17:31:17 +0300

    Facial features to model

--- src/naamataulu/naamataulu/settings.py
+++ src/naamataulu/naamataulu/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DJANGO_DEBUG']
 
-ALLOWED_HOSTS = ['naamataulu-backend.herokuapp.com']
+ALLOWED_HOSTS = ['naamataulu-backend.herokuapp.com', 'localhost']
 
 
 # Application definition
