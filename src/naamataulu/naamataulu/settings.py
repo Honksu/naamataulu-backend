8eb8192ce39a5cd3f229c4783bb223bf5dad9eb7
Antti Kallonen <kallonen.antti@gmail.com>
2018-11-03 16:57:09 +0200

    api.wackymemes.com

--- src/naamataulu/naamataulu/settings.py
+++ src/naamataulu/naamataulu/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DJANGO_DEBUG']
 
-ALLOWED_HOSTS = ['naamataulu-backend.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['naamataulu-backend.herokuapp.com', 'api.wackymemes.com', 'localhost']
 
 
 # Application definition
