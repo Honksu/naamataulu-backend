e728c8d9d1d07a41719b2e2ec04d6dc0505b1261
Antti Kallonen <kallonen.antti@gmail.com>
2018-11-23 14:32:57 +0200

    Test with single user enrolled

--- src/naamataulu/api/tests/test_api_requests.py
+++ src/naamataulu/api/tests/test_api_requests.py
@@ -1,3 +1,7 @@
+import os
+import glob
+from random import shuffle
+
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory, APIClient, RequestsClient
 
@@ -22,8 +26,54 @@ class UserTestCase(TestCase):
         self.assertEqual(response.data['username'], 'Markku')
 
     def test_user_enroll(self):
-        print(User.objects.all())
-        request = self.factory.post('/api/v1/users/1')
-        view = UserViewSet.as_view({'post': 'enroll'})
-        response = view(request, pk=1)
-        self.assertEqual(response.status_code, 200)
+        face_dir = os.path.join('api', 'tests', 'faces', '*.jpg')
+        faces = glob.glob(face_dir)
+
+        with open(faces[0]), 'rb') as data:
+            request = self.factory.post('/api/v1/users/1', {'faces': data}, format='multipart')
+            view = UserViewSet.as_view({'post': 'enroll'})
+            response = view(request, pk=1)
+            self.assertEqual(response.status_code, 200)
+
+        shuffle(faces)
+
+        faces = faces[:100]
+
+        false_negatives = 0
+        false_positives = 0
+        successful = 0
+        not_recognized = 0
+        no_face = 0
+
+        no_face_imgs = []
+        not_recognized_imgs = []
+
+        for face in faces:
+            with open(face, 'rb') as data:
+                request = self.factory.post('/api/v1/users', {'faces': data}, format='multipart')
+                view = UserViewSet.as_view({'post': 'recognize'})
+                try:
+                    response = view(request)
+                    if response.status_code == 200:
+                        if response.data['id'] == 1:
+                            successful += 1
+                        else:
+                            false_positives += 1
+                    else:   
+                        false_negatives += 1
+                        not_recognized += 1
+                        not_recognized_imgs.append(face)
+
+                except IndexError:
+                    false_negatives += 1
+                    no_face += 1
+                    no_face_imgs.append(face)
+                
+        print('False negatives: %s' % false_negatives)
+        print('False positives: %s' % false_positives)
+        print('Successful: %s' % successful)
+        print('Total: %s' % len(faces))
+        print('Not recognized: %s, No faces detected: %s' % (not_recognized, no_face))
+
+        print('Not recognized: %s' % not_recognized_imgs)
+        print('No face: %s' % no_face_imgs)
