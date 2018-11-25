dac970d7b5a5510fa17d3e854c4dd330cfc8e2d1
Antti Kallonen <kallonen.antti@gmail.com>
2018-11-25 20:53:53 +0200

    Face features not required

--- src/naamataulu/api/models.py
+++ src/naamataulu/api/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=64)
-    face_features = models.TextField(null=True)
-    face_recognition_implementer = models.CharField(null=True, max_length=64)
+    face_features = models.TextField(null=True, blank=True)
+    face_recognition_implementer = models.CharField(null=True, max_length=64, blank=True)
