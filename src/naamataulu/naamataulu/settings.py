1e9752073ff55e94a19e08efb2cbec1cf7f604f2
Antti Kallonen <kallonen.antti@gmail.com>
2019-01-30 15:23:45 +0200

    Configure multiple ALLOWED_HOSTS  in .env

--- src/naamataulu/naamataulu/settings.py
+++ src/naamataulu/naamataulu/settings.py
@@ -23,8 +23,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ['DJANGO_DEBUG'] == '1') 
 
-ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
-
+ALLOWED_HOSTS = os.environ['ALLOWED_HOST'].split(',')
 
 # Application definition
 
